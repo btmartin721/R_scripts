837a12456fee7c08309c01b688a88918f2688ad0
btmartin721 <btm002@email.uark.edu>
2018-07-02 17:11:26 -0500

    Added Chi-Square to summary table

--- parseDtests.R
+++ parseDtests.R
@@ -19,19 +19,21 @@ zfilenames <- list.files(pattern="*.z")
 popPvalue=list()
 popz=list()
 
+# *.z files produced from CompD
 for (i in 1:length(zfilenames)) {
   
   zfiles <- read.delim(zfilenames[i], header=TRUE, sep="\t")
   
   Pvalue <- get.p.values(zfiles[[2]])
-  popPvalue[i] <- Pvalue
-  avgz <- zfiles[[2]]
+  popPvalue[i] <- round(Pvalue, digits = 5)
+  avgz <- round(zfiles[[2]], digits = 5)
   popz[i] <- avgz
   
 }
 
 df_total <- data.frame()
 
+# *.txt file produced from CompD
 for (j in 1:length(filenames)) {
   
   test <- tools::file_path_sans_ext(filenames[j])
@@ -42,29 +44,43 @@ for (j in 1:length(filenames)) {
   newP <- get.p.values(dataset[[12]])
 
   zrange <- get.range(dataset[[12]])
-  Z <- paste0("[", zrange[1], ",", zrange[2],"]")
+  minz <- round(zrange[1], digits = 5)
+  maxz <- round(zrange[2], digits = 5)
+  Z <- paste0("[", minz, ",", maxz,"]")
   
-  drange <- get.range(dataset[[8]])
-  D <- paste0("[", drange[1], ",", drange[2], "]")
+  stdev_d <- sd(dataset[[8]])
+  mean_d <- mean(dataset[[8]])
+  d <- round(mean_d, digits = 5)
+  STDEV_D <- round(stdev_d, digits = 5)
+  D <- paste0(d, " (", STDEV_D, ")")
   
+  # Chi-Square No. Significant/Not Significant tests
+  sigChi <- sum(dataset[[11]] <= 0.05)
+  Chi_sig_notsig <- paste(sigChi, rws, sep="/")
   
+  # If P-value <= 0.05
   sigtests <- sum(newP <= 0.05)
+  # Adds Z.Significant/NotSignificant tests to DataFrame
   sig_notsig <- paste(sigtests, rws, sep="/")
   
+  # Bonferroni Correction from number of tests
   bonferroni <- 0.05 / rws
   
+  # P-value <= Bonferroni alpha
   bon_sig <- sum(newP <= bonferroni)
+  #Adds Sig/NotSig to DataFrame
   bonsig_notsig <- paste(bon_sig, rws, sep="/")
   
-  df <- data.frame(test, D, Z, sig_notsig, bonsig_notsig, popz[j], popPvalue[j], stringsAsFactors = FALSE)
+  df <- data.frame(test, D, Z, Chi_sig_notsig, sig_notsig, round(bonferroni, digits = 5), bonsig_notsig, popz[j], popPvalue[j], stringsAsFactors = FALSE)
   
-  colnames(df) <- c("Test", "D-Range", "Z-Range", "No.Significant", "Bonferroni", "Population-Z", "Pop_P-value")
+  colnames(df) <- c("Test", "Mean.D (STDEV.D)", "Z-Range", "Chi-Square.Sig", "Z.Significant", "Bonferroni.Alpha", "Bonferroni.Significant", "Population-Z", "Pop_P-value")
   row.names(df_total) <- NULL
   
   df_total <- rbind(df_total, df)
   
 }
 
+# Outfile to write to. Will overwrite if script is run again.
 ofile <- "summary.tsv"
 
 write.table(df_total, file=ofile, quote=FALSE, sep="\t", col.names=TRUE, row.names=FALSE)
