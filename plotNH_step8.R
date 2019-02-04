910198e6e364cd58b0b5104faa2b38f86926d5c7
Bradley Martin <btm002@email.uark.edu>
2019-02-03 23:05:16 -0600

    scripts to run hybriddetective with parallelNH

---
+++ plotNH_step8.R
@@ -0,0 +1,21 @@
+library("hybriddetective")
+library("genepopedit")
+
+
+###*** SET WORKING DIRECTORY HERE ***###
+
+path.hold <- getwd()
+
+##############################################################################
+
+###*** SETTINGS ***###
+
+pofz <- "/final_run/postSim_combined/NH.Results/GUTT_combined_Zed.txt_Results/GUTT_combined_Zed.txt_PofZ.txt"
+
+
+###*** END OF SETTINGS ***###
+
+##############################################################################
+
+
+nh_plotR(paste0(new_path, pofz))
